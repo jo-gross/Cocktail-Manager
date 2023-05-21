7da57797ace55e0ded4989139d42f51617ddb8be
Johannes Groß <mail@gross-johannes.de>
2023-05-22 00:23:52 +0200

    Fixed conalco import

--- pages/api/scraper/ingredient.tsx
+++ pages/api/scraper/ingredient.tsx
@@ -7,11 +7,23 @@ import JSSoup from 'jssoup';
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     if (req.query?.url?.includes('conalco.de')) {
+      console.log(req.query.url);
       const response = await fetch(req.query.url as string);
+      console.log(response.status + ' ' + response.statusText);
       const body = await response.text();
       const soup = new JSSoup(body);
-      const imageResponse = await fetch(soup.find('div', 'product--image-container').contents[0].find('img').attrs.src);
-      const image = 'data:image/jpg;base64,' + Buffer.from(await imageResponse.arrayBuffer()).toString('base64');
+
+      const imageResponse = await fetch(
+        soup.find('div', 'image-slider--container').contents[0].find('img').attrs.src,
+      ).catch((error) => {
+        console.log(error);
+        return undefined;
+      });
+
+      const image =
+        imageResponse != undefined
+          ? 'data:image/jpg;base64,' + Buffer.from(await imageResponse.arrayBuffer()).toString('base64')
+          : undefined;
       const name = soup.find('h1', 'product--title').text.replace('\n', '').trim();
       const price = soup.find('meta', { itemprop: 'price' }).attrs.content;
 
