d0d7bc38571c9fbe435912da32939c93571ba4bb
Johannes Groß <mail@gross-johannes.de>
2023-11-15 18:30:45 +0100

    Fixed export
    Signed-off-by: Johannes Groß <mail@gross-johannes.de>

--- pages/api/workspaces/[workspaceId]/admin/backups/export.tsx
+++ pages/api/workspaces/[workspaceId]/admin/backups/export.tsx
@@ -3,19 +3,66 @@ import prisma from '../../../../../../lib/prisma';
 import { BackupStructure } from './backupStructure';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const workspaceId = req.query.workspaceId as string | undefined;
+
   if (req.method === 'GET') {
+    const cocktailRecipes = await prisma.cocktailRecipe.findMany({ where: { workspaceId } });
+    const cocktailRecipeSteps = await prisma.cocktailRecipeStep.findMany({
+      where: {
+        cocktailRecipeId: {
+          in: cocktailRecipes.map((recipe) => recipe.id),
+        },
+      },
+    });
+
+    const cards = await prisma.cocktailCard.findMany({ where: { workspaceId } });
+    const cardGroups = await prisma.cocktailCardGroup.findMany({
+      where: {
+        cocktailCardId: {
+          in: cards.map((card) => card.id),
+        },
+      },
+    });
+
+    const calculations = await prisma.cocktailCalculation.findMany({ where: { workspaceId } });
+
     const backup: BackupStructure = {
-      garnish: await prisma.garnish.findMany(),
-      ingredient: await prisma.ingredient.findMany(),
-      glass: await prisma.glass.findMany(),
-      cocktailRecipe: await prisma.cocktailRecipe.findMany(),
-      cocktailRecipeStep: await prisma.cocktailRecipeStep.findMany(),
-      cocktailRecipeGarnish: await prisma.cocktailRecipeGarnish.findMany(),
-      cocktailRecipeIngredient: await prisma.cocktailRecipeIngredient.findMany(),
-      cocktailCard: await prisma.cocktailCard.findMany(),
-      cocktailCardGroup: await prisma.cocktailCardGroup.findMany(),
-      cocktailCardGroupItem: await prisma.cocktailCardGroupItem.findMany(),
-      calculation: await prisma.cocktailCalculation.findMany(),
+      garnish: await prisma.garnish.findMany({ where: { workspaceId } }),
+      ingredient: await prisma.ingredient.findMany({ where: { workspaceId } }),
+      glass: await prisma.glass.findMany({ where: { workspaceId } }),
+      cocktailRecipe: cocktailRecipes,
+      cocktailRecipeStep: cocktailRecipeSteps,
+      cocktailRecipeGarnish: await prisma.cocktailRecipeGarnish.findMany({
+        where: {
+          cocktailRecipeId: {
+            in: cocktailRecipes.map((recipe) => recipe.id),
+          },
+        },
+      }),
+      cocktailRecipeIngredient: await prisma.cocktailRecipeIngredient.findMany({
+        where: {
+          cocktailRecipeStepId: {
+            in: cocktailRecipeSteps.map((step) => step.id),
+          },
+        },
+      }),
+      cocktailCard: cards,
+      cocktailCardGroup: cardGroups,
+      cocktailCardGroupItem: await prisma.cocktailCardGroupItem.findMany({
+        where: {
+          cocktailCardGroupId: {
+            in: cardGroups.map((group) => group.id),
+          },
+        },
+      }),
+      calculation: calculations,
+      calculationItems: await prisma.cocktailCalculationItems.findMany({
+        where: {
+          calculationId: {
+            in: calculations.map((calculation) => calculation.id),
+          },
+        },
+      }),
     };
 
     return res.json(backup);
