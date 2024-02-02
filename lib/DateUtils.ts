be3e712e43643a0fda085198c3e439361d9eb1ef
Johannes Groß <mail@gross-johannes.de>
2024-02-02 23:04:00 +0100

    Added bar chart
    Signed-off-by: Johannes Groß <mail@gross-johannes.de>

--- lib/DateUtils.ts
+++ lib/DateUtils.ts
@@ -3,6 +3,10 @@ import 'react';
 declare global {
   interface Date {
     withoutTime(): Date;
+
+    toFormatDateTimeString(): String;
+
+    toFormatDateString(): String;
   }
 }
 
@@ -10,4 +14,24 @@ Date.prototype.withoutTime = function () {
   const d = new Date(this);
   d.setHours(0, 0, 0, 0);
   return d;
+};
+
+Date.prototype.toFormatDateTimeString = function () {
+  // format date to dd.mm.yyyy hh:mm
+  const d = new Date(this);
+  const day = d.getDate().toString().padStart(2, '0');
+  const month = (d.getMonth() + 1).toString().padStart(2, '0');
+  const year = d.getFullYear();
+  const hours = d.getHours().toString().padStart(2, '0');
+  const minutes = d.getMinutes().toString().padStart(2, '0');
+  return `${day}.${month}.${year} ${hours}:${minutes}`;
+};
+
+Date.prototype.toFormatDateString = function () {
+  // format date to dd.mm.yyyy hh:mm
+  const d = new Date(this);
+  const day = d.getDate().toString().padStart(2, '0');
+  const month = (d.getMonth() + 1).toString().padStart(2, '0');
+  const year = d.getFullYear();
+  return `${day}.${month}.${year}`;
 };
--- pages/workspaces/[workspaceId]/manage/statistics/index.tsx
+++ pages/workspaces/[workspaceId]/manage/statistics/index.tsx
@@ -6,6 +6,13 @@ import { alertService } from '../../../../../lib/alertService';
 import { FaSyncAlt, FaTrashAlt } from 'react-icons/fa';
 import _ from 'lodash';
 import { UserContext } from '../../../../../lib/context/UserContextProvider';
+import { ArcElement, CategoryScale, Legend, LinearScale, TimeScale, Tooltip } from 'chart.js';
+import { Chart as ChartJS } from 'chart.js/auto';
+import { Bar, Doughnut } from 'react-chartjs-2';
+import '../../../../../lib/DateUtils';
+import '../../../../../lib/StringUtils';
+
+ChartJS.register(ArcElement, Tooltip, Legend, TimeScale, CategoryScale, LinearScale);
 
 export default function StatisticsPage() {
   const router = useRouter();
@@ -19,6 +26,7 @@ export default function StatisticsPage() {
   const [loading, setLoading] = useState(false);
 
   const refreshStatistics = useCallback(async () => {
+    if (!workspaceId) return;
     if (loading) return;
     try {
       setLoading(true);
@@ -41,6 +49,52 @@ export default function StatisticsPage() {
   useEffect(() => {
     refreshStatistics();
   }, [startDate, endDate]);
+
+  interface ProcessedData {
+    labels: string[];
+    datasets: {
+      label: string;
+      data: number[];
+      backgroundColor: string;
+    }[];
+  }
+
+  function processData(data: CocktailStatisticItemFull[]): ProcessedData {
+    const hourlyCocktails: Record<string, Record<string, number>> = {};
+
+    data.forEach((entry) => {
+      const date = new Date(entry.date);
+      // Runden der Stunden für den Zeitslot
+      const hour = date.getHours();
+      const dateString = date.toFormatDateString();
+      const formattedHour = `${dateString} ${hour}:00 - ${hour + 1}:00`;
+      const cocktailName = entry.cocktail.name;
+
+      if (!hourlyCocktails[formattedHour]) {
+        hourlyCocktails[formattedHour] = {};
+      }
+      if (!hourlyCocktails[formattedHour][cocktailName]) {
+        hourlyCocktails[formattedHour][cocktailName] = 0;
+      }
+
+      hourlyCocktails[formattedHour][cocktailName]++;
+    });
+
+    const labels = Object.keys(hourlyCocktails).sort();
+    const cocktailNames = new Set<string>();
+
+    Object.values(hourlyCocktails).forEach((cocktails) => {
+      Object.keys(cocktails).forEach((name) => cocktailNames.add(name));
+    });
+
+    const datasets = Array.from(cocktailNames).map((cocktailName) => ({
+      label: cocktailName,
+      data: labels.map((hour) => hourlyCocktails[hour][cocktailName] || 0),
+      backgroundColor: '#' + Math.floor(Math.random() * 16777215).toString(16), // Zufällige Farbe
+    }));
+
+    return { labels, datasets };
+  }
 
   return (
     <ManageEntityLayout
@@ -85,19 +139,89 @@ export default function StatisticsPage() {
         <div className={'card'}>
           <div className={'card-body'}>
             <div className={'card-title'}>
-              Übersicht ({startDate.toLocaleDateString()} - {endDate.toLocaleDateString()})
+              Übersicht ({startDate.toFormatDateString()} - {endDate.toFormatDateString()})
             </div>
-            {_.chain(cocktailStatisticItems)
-              .groupBy('cocktailId')
-              .map((cocktailStatisticItems, cocktailId) => ({ name: cocktailStatisticItems[0].cocktail.name, count: cocktailStatisticItems.length }))
-              .orderBy('count', 'desc')
-              .map((cocktailStatisticItem) => (
-                <div key={''}>
-                  <span className={'font-bold'}>{cocktailStatisticItem.name}: </span>
-                  <span>{cocktailStatisticItem.count}</span>
+            <div className={'grid grid-cols-2 gap-2'}>
+              <div>
+                <div>
+                  {_.chain(cocktailStatisticItems)
+                    .groupBy('cocktailId')
+                    .map((cocktailStatisticItems, cocktailId) => ({ name: cocktailStatisticItems[0].cocktail.name, count: cocktailStatisticItems.length }))
+                    .orderBy('count', 'desc')
+                    .map((cocktailStatisticItem) => (
+                      <div key={''}>
+                        <span className={'font-bold'}>{cocktailStatisticItem.name}: </span>
+                        <span>{cocktailStatisticItem.count}</span>
+                      </div>
+                    ))
+                    .value()}
                 </div>
-              ))
-              .value()}
+                <div className={'divider-sm'}></div>
+                <div>
+                  <strong>Cocktails in Summe: </strong>
+                  {cocktailStatisticItems.length}
+                </div>
+              </div>
+              <div className={'h-full max-h-96'}>
+                <Doughnut
+                  data={{
+                    labels: _.chain(cocktailStatisticItems)
+                      .groupBy('cocktailId')
+                      .map((cocktailStatisticItems, cocktailId) => ({ name: cocktailStatisticItems[0].cocktail.name, count: cocktailStatisticItems.length }))
+                      .orderBy('count', 'desc')
+                      .map((cocktailStatisticItem) => cocktailStatisticItem.name)
+                      .value(),
+                    datasets: [
+                      {
+                        label: 'Anzahl',
+                        data: _.chain(cocktailStatisticItems)
+                          .groupBy('cocktailId')
+                          .map((cocktailStatisticItems, cocktailId) => ({
+                            name: cocktailStatisticItems[0].cocktail.name,
+                            count: cocktailStatisticItems.length,
+                          }))
+                          .orderBy('count', 'desc')
+                          .map((cocktailStatisticItem) => cocktailStatisticItem.count)
+                          .value(),
+                        backgroundColor: _.chain(cocktailStatisticItems)
+                          .groupBy('cocktailId')
+                          .map((cocktailStatisticItems, cocktailId) => ({
+                            name: cocktailStatisticItems[0].cocktail.name,
+                            count: cocktailStatisticItems.length,
+                          }))
+                          .orderBy('count', 'desc')
+                          .map((cocktailStatisticItem) => cocktailStatisticItem.name.string2color())
+                          .value(),
+                        hoverOffset: 4,
+                      },
+                    ],
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={'card'}>
+          <div className={'card-body'}>
+            <div className={'card-title'}>
+              Übersicht ({startDate.toFormatDateString()} - {endDate.toFormatDateString()})
+            </div>
+            <Bar
+              data={processData(cocktailStatisticItems)}
+              options={{
+                responsive: true,
+                scales: {
+                  x: {
+                    stacked: true,
+                    // type: 'time',
+                  },
+                  y: {
+                    beginAtZero: true,
+                    stacked: true,
+                  },
+                },
+              }}
+            />
           </div>
         </div>
         <div className={'card col-span-12'}>
@@ -138,7 +262,7 @@ export default function StatisticsPage() {
                     })
                     .map((item) => (
                       <tr key={item.id}>
-                        <td>{new Date(item.date).toLocaleString()}</td>
+                        <td>{new Date(item.date).toFormatDateTimeString()}</td>
                         <td>{item.cocktail.name}</td>
                         <td>{item.cocktailCard?.name}</td>
                         <td>{item.user?.name}</td>
