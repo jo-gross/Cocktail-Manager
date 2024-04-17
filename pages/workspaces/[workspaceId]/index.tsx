dfa35f75c0d6e0c2ee16890260db8bebd31ec065
Johannes Groß <mail@gross-johannes.de>
2024-04-17 11:03:07 +0200

    feat: Added search shortcut to card page (#220)
    Signed-off-by: Johannes Groß <mail@gross-johannes.de>

--- pages/workspaces/[workspaceId]/index.tsx
+++ pages/workspaces/[workspaceId]/index.tsx
@@ -32,6 +32,19 @@ export default function OverviewPage() {
   const [loadingCards, setLoadingCards] = useState(true);
   const [loadingGroups, setLoadingGroups] = useState(false);
 
+  useEffect(() => {
+    const handleSearchShortCut = (event: any) => {
+      if (event.shiftKey && event.key === 'F') {
+        modalContext.openModal(<SearchModal showStatisticActions={showStatisticActions} />);
+      }
+    };
+    window.addEventListener('keypress', handleSearchShortCut);
+
+    return () => {
+      window.removeEventListener('keypress', handleSearchShortCut);
+    };
+  }, [modalContext, showStatisticActions]);
+
   // fetch cards initially
   useEffect(() => {
     if (!workspaceId) return;
@@ -411,11 +424,13 @@ export default function OverviewPage() {
 
           <>
             {selectedCardId != 'search' && selectedCardId != undefined ? (
-              <div
-                className={'btn btn-square btn-primary rounded-xl md:btn-lg'}
-                onClick={() => modalContext.openModal(<SearchModal showStatisticActions={showStatisticActions} />)}
-              >
-                <FaSearch />
+              <div className={'tooltip'} data-tip={'Suche (Shift + F)'}>
+                <div
+                  className={'btn btn-square btn-primary rounded-xl md:btn-lg'}
+                  onClick={() => modalContext.openModal(<SearchModal showStatisticActions={showStatisticActions} />)}
+                >
+                  <FaSearch />
+                </div>
               </div>
             ) : (
               <></>
