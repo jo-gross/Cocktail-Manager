6f172bfe9c6c942e34d18289e36433d95557f829
Johannes Groß <mail@gross-johannes.de>
2024-09-13 09:38:04 +0200

    fix: navigation bug if previous site was external
    Signed-off-by: Johannes Groß <mail@gross-johannes.de>

--- lib/RouterUtils.ts
+++ lib/RouterUtils.ts
@@ -1,7 +1,7 @@
 import { NextRouter } from 'next/router';
 
 export const routerConditionalBack = async (router: NextRouter, fallbackUrl: string) => {
-  if (window.history.length > 1) {
+  if (window.history.length > 1 && document.referrer.includes(window.location.origin)) {
     router.back();
   } else {
     await router.replace(fallbackUrl);
