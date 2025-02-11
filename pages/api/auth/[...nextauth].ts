d6f60f0c14aa14e1fde4ebfb66ba95355fed655d
Jakob Mueller <me@jakobm.de>
2025-02-11 09:25:56 +0100

    feat: Add option for custom OIDC provider
    Currently only Google is supported.
    With the additional custom OIDC provider, all compliant auth providers can be used.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,12 +2,14 @@ import NextAuth, { NextAuthOptions } from 'next-auth';
 import Google from 'next-auth/providers/google';
 import prisma from '../../../prisma/prisma';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { OAuthConfig } from 'next-auth/providers';
 
-export const authOptions: NextAuthOptions = {
-  theme: {
-    logo: '/images/Logo.svg',
-  },
-  providers: [
+const providers: OAuthConfig<any>[] = [];
+
+// Google OAuth Provider
+// Required: GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
+if (process.env.GOOGLE_CLIENT_ID as string) {
+  providers.push(
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
@@ -15,7 +17,37 @@ export const authOptions: NextAuthOptions = {
         timeout: 40000,
       },
     }),
-  ],
+  );
+}
+
+// Custom OIDC Provider
+// Required: CUSTOM_OIDC_NAME, CUSTOM_OIDC_ISSUER_URL, CUSTOM_OIDC_CLIENT_ID[, CUSTOM_OIDC_CLIENT_SECRET]
+if (process.env.CUSTOM_OIDC_NAME as string) {
+  providers.push({
+    id: 'custom_oidc',
+    name: process.env.CUSTOM_OIDC_NAME as string,
+    type: 'oauth',
+    wellKnown: process.env.CUSTOM_OIDC_ISSUER_URL as string,
+    authorization: { params: { scope: process.env.CUSTOM_OIDC_SCOPES || 'openid email profile' } },
+    clientId: process.env.CUSTOM_OIDC_CLIENT_ID as string,
+    clientSecret: process.env.CUSTOM_OIDC_CLIENT_SECRET as string,
+    idToken: true,
+    checks: ['pkce', 'state'],
+    profile(profile) {
+      return {
+        id: profile[(process.env.CUSTOM_OIDC_ID_KEY as string) || 'sub'],
+        name: profile[(process.env.CUSTOM_OIDC_NAME_KEY as string) || 'name'],
+        email: profile[(process.env.CUSTOM_OIDC_EMAIL_KEY as string) || 'email'],
+      };
+    },
+  });
+}
+
+export const authOptions: NextAuthOptions = {
+  theme: {
+    logo: '/images/Logo.svg',
+  },
+  providers,
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
