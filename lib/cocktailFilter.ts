a9579ef13e5e3ae8137b117f1a54d141786d2f57
Johannes Groß <11421371+jo-gross@users.noreply.github.com>
2026-02-22 13:17:45 +0100

    fix: cocktail overview filter (#917)

--- lib/cocktailFilter.ts
+++ lib/cocktailFilter.ts
@@ -5,11 +5,20 @@ export const cocktailFilter = (filterString: string) => {
   const filterFor = normalizeString(filterString);
 
   return function (cocktailRecipe: CocktailRecipeModel): boolean {
+    const isLongSearch = filterFor.length >= 3;
+
     return (
       normalizeString(cocktailRecipe.name).includes(filterFor) ||
-      normalizeString(cocktailRecipe.glass?.name).includes(filterFor) ||
-      cocktailRecipe.garnishes.some((garnish) => normalizeString(garnish.garnish.name).includes(filterFor)) ||
-      cocktailRecipe.tags.some((tag) => normalizeString(tag).includes(filterFor))
+      (normalizeString(cocktailRecipe.glass?.name).includes(filterFor) && isLongSearch) ||
+      (cocktailRecipe.garnishes.some((garnish) => normalizeString(garnish.garnish.name).includes(filterFor)) && isLongSearch) ||
+      (cocktailRecipe.tags.some((tag) => normalizeString(tag).includes(filterFor)) && isLongSearch) ||
+      (cocktailRecipe.steps ?? []).some((step) =>
+        step.ingredients.some(
+          (ingredient) =>
+            (normalizeString(ingredient.ingredient?.name).includes(filterFor) && isLongSearch) ||
+            (normalizeString(ingredient.ingredient?.shortName ?? undefined).includes(filterFor) && isLongSearch),
+        ),
+      )
     );
   };
 };
